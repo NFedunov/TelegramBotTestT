08fc2e7299c109edc0b07fc5087afb85999754a2
NFedunov <adsfsdf>
2019-07-24 16:54:55 +0300

    Init commit with first version of test task

---
+++ actions_impl.py
@@ -0,0 +1,21 @@
+#Реализация функций реагирования
+
+def action1():
+    print("Action1")
+    return "Action 1 done!"
+
+def action2():
+    print("Action2")
+    return "Action 2 done"
+
+def action3():
+    print("Action3")
+    return "Action 3 done"
+
+def action4():
+    print("Action4")
+    return "Action 4 done"
+
+def action5():
+    print("Action5")
+    return "Action 5 done"
---
+++ main.py
@@ -0,0 +1,99 @@
+import telebot
+from datetime import time, datetime
+from time import sleep
+import actions_impl
+
+#Инициализация бота
+bot = telebot.TeleBot("TOKEN")
+
+#id пользователя, которому будет приходить уведомление
+#получается после запуска скрипта отправкой команды /start боту
+worker_id = 0
+
+#Время проверки (час, минута, секунда)
+check_time = time(16, 6, 0, 0)
+
+#функция для проверки БД
+def check_db_data():
+    return {"code":12, "message":"Empty values"}
+
+#словарь ошибок
+#Ключ - кол ошибки, значения - предлгаемые действия
+#0 позиция - название вызываемой функции, 1 позиция - описание
+dict_of_actions = \
+    {12: [
+        ["action1", "Do action 1"],
+        ["action2", "Do action 2"],
+        ["action3", "Do action 3"]
+    ],
+     13: [
+        ["action4", "Do action 4"],
+        ["action5", "Do action 5"]
+     ]
+
+    }
+
+#Добавление кнопок выбора действий в клавиатуру
+def add_keys(keyboard, error_code):
+    actions = dict_of_actions[error_code]
+    for action in actions:
+        key = telebot.types.InlineKeyboardButton(text=action[1], callback_data=action[0])
+        keyboard.add(key)
+
+#Получение и вызов функции для выполнения выбора пользователя (добавить реализацию аргументов функции?)
+def call_action(action):
+    try:
+        action_func = getattr(actions_impl, action.lower())
+    except AttributeError:
+        bot.send_message(worker_id, f"Неудалось выполнить действие {action}. "
+        f"Возможно функция реагирования не реализована")
+        return "Error"
+    return action_func()
+
+
+#Функция для проверки БД, получения возможных действий и отправки результатов в телегу
+def send_bd_check_results():
+    check_result = check_db_data()
+    keyboard = telebot.types.InlineKeyboardMarkup()
+    add_keys(keyboard, check_result["code"])
+    bot.send_message(worker_id, f"Результаты проверки базы данных:\n\n"
+        f"Код ошибки: {check_result['code']}\n"
+        f"Сообщение:  {check_result['message']}",
+        reply_markup = keyboard)
+    bot.polling(none_stop=True, interval=5)
+
+#Обработчик команды /start
+@bot.message_handler(commands=['start'])
+def get_worker_id(message):
+    global worker_id
+    worker_id = message.from_user.id
+    bot.stop_polling()
+
+#Обработчик выбора пользователя
+@bot.callback_query_handler(func=lambda call: True)
+def get_action(call):
+    result = call_action(call.data)
+    bot.send_message(worker_id, f"Результат выполнения: {result}")
+    bot.stop_polling()
+
+#Функция для проверки времени
+def is_check_time():
+    print(f"Time: {datetime.now().time()}")
+    now = datetime.now().time()
+    #если просто сравнить, то сравниваются и миллисекунды
+    if now.hour == check_time.hour and now.minute == check_time.minute and now.second == check_time.second:
+        return True
+    else:
+        return True#False
+
+def __main__():
+    #Ожидание команды /start
+    bot.polling(none_stop=True, interval=5)
+    bot.send_message(worker_id, f"Готово, проверка базы данных будет выполнена в {check_time}")
+    #Пока не пришло вермя проверки БД спим
+    while not is_check_time():
+        sleep(1)
+    send_bd_check_results()
+    return
+
+__main__()
